4e9a03571d4756ea6613f7e42e2f0b12f5e047e2
Takayuki Sato <sttk.xslet@gmail.com>
2026-07-24 14:14:33 +0900

    update: removed getter and setter of a context from DataAcc (#110)

--- data-acc.go
+++ data-acc.go
@@ -5,23 +5,14 @@
 package sabi
 
 import (
-	"context"
-
 	"github.com/sttk/errs"
 )
 
-// DataAcc is an interface that provides access to data connections and execution context
+// DataAcc is an interface that provides access to data connections
 // within a transactional or logic execution scope. It acts as an abstraction layer for
 // retrieving active database or resource connections by name and type, allowing data access
 // objects or repositories to interact with resources without knowing the details of the
 // underlying transaction coordinator.
 type DataAcc interface {
 	getDataConn(name, dataConnType string) (DataConn, errs.Err)
-
-	setContext(ctx context.Context)
-
-	// Context returns the context.Context associated with the current execution scope,
-	// which can be used to control timeouts, cancellation, or pass request-scoped values
-	// down to data connections or queries.
-	Context() context.Context
 }
